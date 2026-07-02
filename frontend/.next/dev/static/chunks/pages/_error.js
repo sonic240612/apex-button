8351b4f2c4875ec82cceb3514b728494e8b391f6
sonic240612 <citrus@Citrusui-Macmini.local>
2026-07-02 17:47:48 +0900

    Refactor: Move frontend to apex-button and fix persistence logic

---
+++ frontend/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/1ikz_next_dist_compiled_146e-u-._.js",
+  "static/chunks/1ikz_next_dist_shared_lib_0g27g3w._.js",
+  "static/chunks/1ikz_next_dist_client_1pka1lv._.js",
+  "static/chunks/1ikz_next_dist_0772do9._.js",
+  "static/chunks/1m03_next_dist_pages__error_0unsd7d.js",
+  "static/chunks/[next]_entry_page-loader_ts_0q8tv9j._.js",
+  "static/chunks/1ikz_react-dom_1nj-ool._.js",
+  "static/chunks/1ikz_0wvt9xp._.js",
+  "static/chunks/[root-of-the-server]__0vm2mor._.js",
+  "static/chunks/[project]_Storage_Workspace_The_only_button_frontend_pages__error_0du2_q-._.js",
+  "static/chunks/06w1_Storage_Workspace_The_only_button_frontend_pages__error_1n0k9js._.js"
+])
